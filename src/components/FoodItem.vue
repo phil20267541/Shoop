701b7027a0e5f5c5955b9b737472e318789ace30
Phil <phillipswann2008@gmail.com>
2025-10-14 22:00:23 +0100

    Practice Project

---
+++ src/components/FoodItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>
+      {{ foodName }}
+      <img src="..\..\public\favicon.ico" v-show="isFavorite">
+    </h2>
+    <p>{{ foodDesc }}</p>
+    <button v-on:click="toggleFavorite">Favorite</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['foodName','foodDesc','isFavorite'],
+    emits: ['toggle-favorite'],
+    methods: {
+      toggleFavorite() {
+        this.$emit('toggle-favorite', this.foodName);
+      }
+    }
+  }
+</script>
+
+<style>
+  img {
+    height: 1.5em;
+    float: right;
+  }
+</style>
