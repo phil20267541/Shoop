26b065b1a202d565f4a34e7e7b95270aa4df59db
Phil <phillipswann2008@gmail.com>
2025-11-12 10:17:12 +0000

    Footer Work

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,43 @@
     <div class="sign-up-container">
       <p>Sign Up Here</p>
     </div>
-    <div class="links-container">
+    <div class="quick-links-container">
       <p>Quick Links</p>
+      <div class="links-container">
+        <FooterItem v-for="page in pages" :key="page.title" :item="page" />
+      </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import FooterItem from './FooterItem.vue'
+
+export default {
+  components: { FooterItem },
+  data() {
+    return {
+      pages: [
+        {title: "My Shoop", link: "/ShoopPet", subpages: [
+          {title: "My Shoop", link: "/ShoopPet"},
+          {title: "Stats", link: "/ShoopStats"}
+        ]},
+        {title: "Shop", link: "/Shop", subpages: [
+          {title: 'Shop', link: '/Shop'},
+          {title: 'Hats', link: '/Shop/hats'},
+          {title: 'Tops', link: '/Shop/tops'},
+          {title: 'Boots', link: '/Shop/boots'},
+          {title: 'Colors', link: '/Shop/colors'},
+          {title: 'Themes', link: '/Shop/themes'}
+        ]},
+        {title: "Adopt", link: "/ShoopFarm"},
+        {title: "Wiki", link: "/Wiki"}
+
+      ]
+    };
+  }
+};
+</script>
 
 <style>
 .footer-container {
@@ -20,5 +50,18 @@
   justify-content: space-around;
 }
 
+.sign-up-container, .quick-links-container {
+  justify-items: center;
+  width: 40vw;
+  p {
+    text-decoration: underline;
+    margin-bottom: 20px;
+  }
+}
+
+.links-container {
+  display: flex;
+  justify-content: space-around;
+}
 
 </style>
---
+++ src/components/FooterItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul v-if="!item.subpages">
+    <li :key="item.title">
+      <router-link :to="item.link">{{ item.title }}</router-link>
+    </li>
+  </ul>
+  <ul v-else>
+    <li v-for="sub in item.subpages" :key="sub.title">
+      <router-link :to="sub.link">{{ sub.title }}</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0 20px;
+  a {
+    color: #F2D6A2;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+}
+
+
+</style>
